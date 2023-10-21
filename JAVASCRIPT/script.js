207da992bae39cc1b297906b2e732252e4f05172
Jackeline Sanchez <sanchezjackeline977@gmail.com>
2023-10-21 13:13:06 -0600

    UPDATE 2.2

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,8 @@
+
+
+const settns = document.getElementById("settns");
+settns.addEventListener("click",()=>{
+    barraLateral.classList.toggle("mini-barra-lateral");
+})
+
+const barraLateral = document.querySelector(".barra-lateral");
